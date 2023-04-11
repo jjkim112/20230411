075d8deed4cb50d9ef09ae61f886a73d0c98682b
jjkim112 <jjkim112@naver.com>
2023-04-11 11:26:02 +0900

    expressroutes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const userRouter = require("./routes/user");
+const tweetRouter = require("./routes/tweet");
 const app = express();
 
 const port = 3010;
 
+app.use("/user", userRouter);
+//경로,컴포넌트연결처럼
+app.use("/tweet", tweetRouter);
+//.use->미들웨어
 app.get("/", (req, res) => {
   res.send("Hello,kai!");
 });
